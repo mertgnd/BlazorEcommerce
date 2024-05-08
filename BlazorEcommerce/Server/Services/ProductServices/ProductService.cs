342c5d3868e9e41de992cbc7e32f5ad83b1bcb03
Mert Gündoğdu <mertgnd36@gmail.com>
2024-05-08 14:43:50 +0200

    Added productTypes and Variants. Added Select box for product variants.

--- BlazorEcommerce/Server/Services/ProductServices/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductServices/ProductService.cs
@@ -13,7 +13,10 @@ namespace BlazorEcommerce.Server.Services.ProductServices
 		public async Task<ServiceResponse<Product>> GetProductByIdAsync(int Id)
 		{
 			var response = new ServiceResponse<Product>();
-			var product = await _context.Products.FindAsync(Id);
+			var product = await _context.Products
+				.Include(p => p.Variants)
+				.ThenInclude(v => v.ProductType)
+				.FirstOrDefaultAsync(p => p.Id == Id);
             if (product == null)
             {
 				response.Success = false;
@@ -31,7 +34,7 @@ namespace BlazorEcommerce.Server.Services.ProductServices
 		{
 			var response = new ServiceResponse<List<Product>> 
 			{
-				Data = await _context.Products.ToListAsync()
+				Data = await _context.Products.Include(p => p.Variants).ToListAsync()
 			};
 
 			return response;
@@ -42,7 +45,9 @@ namespace BlazorEcommerce.Server.Services.ProductServices
 			var response = new ServiceResponse<List<Product>>
 			{
 				Data = await _context.Products
-							.Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower())).ToListAsync()
+							.Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+							.Include(p => p.Variants)
+							.ToListAsync()
 			};
 
 			return response;
